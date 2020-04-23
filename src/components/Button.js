d5264f960b847efc63fc1d4677c3dae2776b18e4
Mandy Arens <mandyfung93@gmail.com>
2020-04-23 14:16:07 -0600

    passed tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-
    let buttonClass = "button";
-
    if (props.confirm) {
       buttonClass += " button--confirm";
    }
-   if (props.danger){
+   if (props.danger) {
       buttonClass += " button--danger";
    }
-   return <button className={buttonClass}>{props.children}</button>;
+
+   return(
+      <button 
+         className={buttonClass} 
+         onClick={props.onClick} 
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>);
 }
