f21648ec0cfc69ef4bff410e86758022ab2ccb54
Mandy Arens <mandyfung93@gmail.com>
2020-04-25 14:13:11 -0600

    added hover state

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/DayListItem.scss"
 
 const classNames = require('classnames');
 
 export default function DayListItem(props) {
+  const [hover, setHover] = useState(false);
+  const toggleHover = () => setHover(!hover);
+
   const dayClass = classNames({
+    'day-list__item': true,
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   })
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
