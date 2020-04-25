27a45a3de932ecebf3c2cb1deb35fd3116833a6c
Mandy Arens <mandyfung93@gmail.com>
2020-04-25 12:05:32 -0600

    changed day list item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function DayListItem(props){
-  return(
-    <li>
-      <h2 className="text--regular"></h2>
-      <h3 className="text--light"></h3>
+export default function DayListItem(props) {
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   )
 }
