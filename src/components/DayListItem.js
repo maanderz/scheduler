03262396df0cc3efe2cdc490ffa922a72960495b
Mandy Arens <mandyfung93@gmail.com>
2020-04-25 13:39:43 -0600

    added selected class to component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "components/DayListItem.scss"
+
+const classNames = require('classnames');
 
 export default function DayListItem(props) {
+  const dayClass = classNames({
+    'day-list__item--selected': props.selected
+  })
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
