ed6c3baf7686e2541ed8cc7b69d70ce60be3baeb
Mandy Arens <mandyfung93@gmail.com>
2020-04-30 13:53:04 -0600

    added interview list

--- src/components/InterviewList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import InterviewListItem from "components/InterviewListItem";
-
-export default function InterviewList(props) {
-  return (
-    <ul>
-    </ul>
-  )
-}
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  return (
+    <ul>
+    </ul>
+  )
+}
