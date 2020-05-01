bd0878b856e1bf499f674ea4630aa5cbcca2e8c0
Mandy Arens <mandyfung93@gmail.com>
2020-05-01 12:33:45 -0600

    added map

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "components/InterviewerListItem";
+import action from "@storybook/addon-actions/dist/preview/action";
 
 export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        <InterviewerListItem
-          id={props.interviewers.id}
-          name={props.interviewers.name}
-          avatar={props.interviewers.avatar}
-        />
+        {props.interviewers.map(p => <InterviewerListItem
+          key={p.id}
+          name={p.name}
+          avatar={p.avatar}
+          setInterviewer={action("setInterviewer")} />)}
       </ul>
     </section>
   )
