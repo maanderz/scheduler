e9205066bf9c5bda04b4bad4d2906befdd5dd0a2
Mandy Arens <mandyfung93@gmail.com>
2020-05-01 12:30:49 -0600

    added prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,7 +6,13 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        <InterviewerListItem
+          id={props.interviewers.id}
+          name={props.interviewers.name}
+          avatar={props.interviewers.avatar}
+        />
+      </ul>
     </section>
   )
 }
