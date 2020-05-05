c136b6ea1d0d35d5e4a5fec5a386c07116bdc2d4
Mandy Arens <mandyfung93@gmail.com>
2020-05-05 12:33:01 -0600

    added show component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -136,3 +137,4 @@ storiesOf("Appointment", module)
   .add("Appointment time", () => <Appointment time="12pm"/>)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show />)
