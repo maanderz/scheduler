d8e67134b30205ea85f7b031c85cd2caed568f39
Mandy Arens <mandyfung93@gmail.com>
2020-05-14 11:36:12 -0600

    added appointment logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,15 @@ import Empty from "components/Appointment/Empty";
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+          <Empty />
+        )}
     </article>
   )
 }
